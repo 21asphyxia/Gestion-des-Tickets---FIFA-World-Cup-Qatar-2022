2346d8073bfc73ccf3c7d7340ef97aacdd3b56fc
Salah <salaheddinehanaoui@gmail.com>
2022-12-06 10:04:32 +0100

    finished admin includes navbar sidebar head corejs

---
+++ includes/admin/corejs.php
@@ -0,0 +1,7 @@
+    <!--   Core JS Files   -->
+    <script src="../../assets/js/template/core/popper.min.js"></script>
+    <script src="../../assets/js/template/core/bootstrap.min.js"></script>
+    <script src="../../assets/js/template/plugins/perfect-scrollbar.min.js"></script>
+    <script src="../../assets/js/template/plugins/smooth-scrollbar.min.js"></script>
+    <script src="../../assets/js/template/plugins/chartjs.min.js"></script>
+    <script src="../../assets/js/scripts.js"></script>
