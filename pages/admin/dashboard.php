0698551d10d3747aa27eb057920a181e1de3f300
21asphyxia <mouadelamraoui@gmail.com>
2022-12-06 10:37:27 +0100

    updated include files

---
+++ pages/admin/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+include_once '../../includes/admin/head.php';
+?>
+<body class="g-sidenav-show bg-gray-100">
+    <?php include_once '../../includes/admin/sidebar.php'; ?>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <?php include_once '../../includes/admin/navbar.php';?>
+        <!-- Content -->
+    </main>
+</body>
+</html>
+<?php include_once '../../includes/admin/corejs.php'; ?>
+    
+
