d55698d14d9f55c0eb4a95ae07f68b4463d9c944
Salah <salaheddinehanaoui@gmail.com>
2022-12-07 11:40:37 +0100

    .

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,30 @@
 // START SALAH
+        //GRAPH
+if (window.location.pathname=='/Gestion-des-Tickets---FIFA-World-Cup-Qatar-2022/pages/admin/dashboard.php') {
+  var xValues = ["E-Tickets Disponible", "E-Tickets Reservés", "E-Tickets Restants"];
+var yValues = [7400, 5400, 2000];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+];
+
+new Chart("myChart", {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    //
+  }
+});
+}
+
+
 // END SALAH
 
 // START HAJJOU
